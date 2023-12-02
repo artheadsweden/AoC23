09c447aad833416ef1352d1d548d11206b4265f8
Joakim Wassberg <joakim.wassberg@svenskaspel.se>
2023-12-02 11:37:36 +0100

    AoC 23 Day 2

--- day02.py
+++ day02.py
@@ -1,7 +1,49 @@
 from get_puzzle import get_data
+from collections import defaultdict
+
 
 def calculate_parts(file_path):
-    pass
+    with open(file_path, 'r') as file:
+        data = file.read().strip()
+    
+    part1_sum = 0
+    part2_sum = 0
+
+    # Define the maximum number of cubes for each color
+    max_cubes = {'red': 12, 'green': 13, 'blue': 14}
+
+    # Process each line (game) in the input data
+    for line in data.split('\n'):
+        game_valid = True
+        game_id, events = line.split(':')
+        
+        # Initialize a dictionary to keep track of the maximum number of cubes shown per color
+        max_shown_cubes = defaultdict(int)
+
+        # Process each event (subset of cubes shown) in the game
+        for event in events.split(';'):
+            for cube_info in event.split(','):
+                count, color = cube_info.split()
+                count = int(count)
+
+                # Update the maximum number of cubes shown for each color
+                max_shown_cubes[color] = max(max_shown_cubes[color], count)
+
+                # Check if the number of cubes shown exceeds the limit
+                if count > max_cubes[color]:
+                    game_valid = False
+
+        # Calculate the score (power) of the minimum set of cubes
+        score = 1
+        for count in max_shown_cubes.values():
+            score *= count
+        part2_sum += score
+
+        # If the game is valid, add its ID to part1_sum
+        if game_valid:
+            part1_sum += int(game_id.split()[-1])
+
+    return part1_sum, part2_sum
 
 
 get_data(2, 2023)
